6e3855a7ad467505e232381c9fdde5e7e0cc6f92
dvrajput <lifehacker1910@gmail.com>
2025-02-24 22:17:23 +0530

    favicon added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Default Title')</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
